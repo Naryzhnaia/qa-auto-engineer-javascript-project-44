2a2529402ce13b6a07a298386bed1982f4d8fb7d
Anna Naryzhnaia <999222111@inbox.ru>
2025-04-07 00:50:09 +0300

    fix test

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,7 +5,7 @@ import game from "../src/index.js";
 
 const textQuestionEvenOrOdd = (min, max) => {
   const randomInt = getRandomInt(min, max);
-  console.log(`Answer 'yes' if the number is even, otherwise answer 'no'. 
+  console.log(`Answer "yes" if the number is even, otherwise answer "no". 
 Question: ${randomInt}`);
 
   const rightAnswer = randomInt % 2 === 0 ? "yes" : "no";
